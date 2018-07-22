a15c92596ac34d1b93ffb265e284165720459d8d
Stewart ilondanga <stilondanga@gmail.com>
2018-07-22 13:36:44 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ var today = new Date();
 var now = today.getHours();
 var greeting = document.getElementById("greeting");
 var message;
+
+if ( now < 07 ){
+  message = 'Laugh and the world laughs with you, snore and you sleep alone! Good Morning';
+} else if ( now < 08 ) {
+  message = 'Everyday may not be good, but there is something good in every day! Good Morning';
+} else if ( now < 09 ) {
