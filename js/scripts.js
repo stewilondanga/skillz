a33e96688be40a1a4543cbeb1a75ab8f1b9603d8
Stewart ilondanga <stilondanga@gmail.com>
2018-07-22 13:37:19 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,8 @@ if ( now < 07 ){
 } else if ( now < 08 ) {
   message = 'Everyday may not be good, but there is something good in every day! Good Morning';
 } else if ( now < 09 ) {
+	message = 'If at first you do not succeed, find out if the loser gets anything. Good Morning';
+} else if ( now < 10 ) {
+  message = 'There are lots of people who mistake their imagination for their memory. Good Morning';
+} else if ( now < 11 ) {
+  message = 'If I want to knock a story off the front page, I just change my hairstyle. Good Morning';
